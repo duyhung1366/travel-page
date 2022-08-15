1c19927a7123e514c29a15949747d33525f30de0
= <=>
2022-08-16 01:26:53 +0700

    update header

--- src/components/layout/components/Header.tsx
+++ src/components/layout/components/Header.tsx
@@ -1,13 +1,14 @@
-import { Button, Grid } from '@mui/material';
-import './index.scss';
-import React from 'react';
-import { Container } from '@mui/system';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import InstagramIcon from '@mui/icons-material/Instagram';
-import TwitterIcon from '@mui/icons-material/Twitter';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import TelegramIcon from '@mui/icons-material/Telegram';
 import PinterestIcon from '@mui/icons-material/Pinterest';
+import SearchIcon from '@mui/icons-material/Search';
+import TelegramIcon from '@mui/icons-material/Telegram';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import { Box, Grid } from '@mui/material';
+import { Container } from '@mui/system';
+import { Link } from 'react-router-dom';
+import './index.scss';
 
 type Props = {};
 
@@ -17,35 +18,71 @@ const Header = (props: Props) => {
       <div className="Header-container">
         <Container fixed maxWidth="xl">
           <Grid container className="header-first">
-            <Grid item xs={6} sm={8} md={8}>
+            <Grid item xs={8} sm={8} md={6}>
               <span>0383211385</span>
-              <a href="#">
+              <a href="#top">
                 <PinterestIcon sx={{ fontSize: '12px' }} />
               </a>
-              <a href="#">
+              <a href="#top">
                 <FacebookIcon sx={{ fontSize: '12px' }} />
               </a>
-              <a href="#">
+              <a href="#top">
                 <TwitterIcon sx={{ fontSize: '12px' }} />
               </a>
-              <a href="#">
+              <a href="#top">
                 <LinkedInIcon sx={{ fontSize: '12px' }} />
               </a>
-              <a href="#">
+              <a href="#top">
                 <TelegramIcon sx={{ fontSize: '12px' }} />
               </a>
-              <a href="#">
+              <a href="#top">
                 <InstagramIcon sx={{ fontSize: '12px' }} />
               </a>
             </Grid>
-            <Grid item xs={6} sm={4} md={4} sx={{ textAlign: 'right' }}>
-              <a href="#" style={{ borderRight: 'solid 1px #fff' }}>
+            <Grid item xs={4} sm={4} md={6} sx={{ textAlign: 'right' }} className="login">
+              <Link to="/login" style={{ borderRight: 'solid 1px #fff' }}>
                 Login
-              </a>
-              <a href="#">Register</a>
+              </Link>
+              <Link to="/register">Register</Link>
             </Grid>
           </Grid>
-          <div className="header-nav"></div>
+        </Container>
+      </div>
+      <div className="header__nav">
+        <Container
+          fixed
+          maxWidth="xl"
+          sx={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            padding: '20px 0',
+          }}
+        >
+          <div className="header__nav__logo">
+            <a href="#">
+              <img src="https://tranhoangkhang1212.github.io/travelix/assets/images/logo.webp" />
+              <span>Travelix</span>
+            </a>
+          </div>
+          <Box
+            sx={{ display: { md: 'flex', sm: 'none', xs: 'none', lg: 'flex', xl: 'flex' } }}
+            className="header__nav__box"
+          >
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/offers">Offers</Link>
+            <Link to="/news">News</Link>
+            <Link to="/contact">contact</Link>
+          </Box>
+          <Box
+            className="header__nav__search"
+            sx={{ display: { md: 'flex', sm: 'none', xs: 'none', lg: 'flex', xl: 'flex' } }}
+          >
+            <a>
+              <SearchIcon />
+            </a>
+          </Box>
         </Container>
       </div>
     </div>
